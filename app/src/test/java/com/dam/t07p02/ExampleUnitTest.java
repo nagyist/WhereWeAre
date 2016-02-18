df55fb87a92094c6997945469475f7e1c65f3366
Dropee <pedrob1ih@gmail.com>
2016-02-18 22:39:57 +0100

    a

---
+++ app/src/test/java/com/dam/t07p02/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package com.dam.t07p02;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}
